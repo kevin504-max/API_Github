48c6f7ed38f866bf2da76a1be5a109b486b4dc3b
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-04-19 21:36:28 -0300

    Estilizations

--- github_finder/src/components/Search.tsx
+++ github_finder/src/components/Search.tsx
@@ -6,14 +6,16 @@ import { useState } from 'react'
 
 import { BsSearch } from 'react-icons/bs'
 
+import classes from './Search.module.css'
+
 const Search = ({loadUser}: SearchProps) => {
     const [userName, setUserName] = useState('');
     
     return (
-        <div>
+        <div className={classes.search}>
             <h2>Search for an user:</h2>
             <p>Know your main repositories</p>
-            <div>
+            <div className={classes.search_container}>
                 <input 
                     type="text" 
                     placeholder="Enter with a name..." 
--- github_finder/src/routes/Home.tsx
+++ github_finder/src/routes/Home.tsx
@@ -10,13 +10,23 @@ const Home = () => {
     const loadUser = async (userName: string) => {
         const res = await fetch(`https://api.github.com/users/${userName}`);
         const data = await res.json();
+        const { avatar_url, login, location, followers, following } = data;
+        const userData: UserProps = {
+            avatar_url,
+            login,
+            location,
+            followers,
+            following
+        };
 
-        console.log(data);
+        setUser(userData);
+
     }
 
     return (
         <div>
             <Search loadUser={loadUser} />
+            {user && <p>{user.login}</p>}
         </div>
     )
 };
