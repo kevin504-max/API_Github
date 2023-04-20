8b79a9ff027b9eada07380dbc334d180652b584f
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-04-19 22:28:31 -0300

    Estilization User

--- github_finder/src/components/User.tsx
+++ github_finder/src/components/User.tsx
@@ -4,27 +4,29 @@ import { MdLocationPin } from 'react-icons/md'
 
 import { Link } from 'react-router-dom'
 
+import classes from './User.module.css'
+
 const User = ({
     login, avatar_url, followers, following, location 
 }: UserProps) => {
     return (
-        <div>
+        <div className={classes.user}>
             <img src={avatar_url} alt={login} />
             <h2>{login}</h2>
             {location && (
-                <p>
+                <p className={classes.location}>
                     <MdLocationPin />
                     <span>{location}</span>
                 </p>
             )}
-            <div>
+            <div className={classes.stats}>
                 <div>
                     <p>Followers:</p>
-                    <p>{followers}</p>
+                    <p className={classes.number}>{followers}</p>
                 </div>
                 <div>
                     <p>Following:</p>
-                    <p>{following}</p>
+                    <p className={classes.number}>{following}</p>
                 </div>
             </div>
             <Link to={`/repositories/${login}`}>Check best projects</Link>
